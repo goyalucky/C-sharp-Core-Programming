6038c2cea5a53dd1549256ce6c4a8cf17f084a9d
goyallucky <luckygoyal221982@gmail.com>
2025-12-26 10:52:06 +0530

    [Lucky Goyal] committed the review practice problem

---
+++ Review_practice_problem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string s = "aaaabbbccd";
+		int count = 1;
+		System.Text.StringBuilder result = new System.Text.StringBuilder();
+		for (int i = 1; i <= s.Length; i++)
+		{
+			if (i < s.Length && s[i] == s[i - 1])
+			{
+				count++;
+			}
+			else
+			{
+				result.Append(s[i - 1]).Append(count);
+				count = 1;
+			}
+		}
+		Console.WriteLine(result.ToString());
+	}
+}
