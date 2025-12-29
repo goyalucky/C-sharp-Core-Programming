bdcc9e8e1756f31b5aff061b6c54ad3a0eb623dc
Lucky Goyal <122284578+goyalucky@users.noreply.github.com>
2025-12-29 17:00:48 +0530

    Refactor Main method to use string operation classes
    Refactored the Main method to use StringCompression and ReverseStringExcept classes for string operations.

--- Review_practice_problem/Program.cs
+++ Review_practice_problem/Program.cs
@@ -1,24 +1,15 @@
-﻿using System;
+using System;
 
 class Program
 {
 	static void Main(string[] args)
 	{
-		string s = "aaaabbbccd";
-		int count = 1;
-		System.Text.StringBuilder result = new System.Text.StringBuilder();
-		for (int i = 1; i <= s.Length; i++)
-		{
-			if (i < s.Length && s[i] == s[i - 1])
-			{
-				count++;
-			}
-			else
-			{
-				result.Append(s[i - 1]).Append(count);
-				count = 1;
-			}
-		}
-		Console.WriteLine(result.ToString());
+		// Create an object to call the string compression method
+		StringCompression sc = new StringCompression();
+		sc.CompressString();
+
+		// Create an object to call the reverse string method
+		ReverseStringExcept r = new ReverseStringExcept();
+		r.ReverseString();
 	}
 }
